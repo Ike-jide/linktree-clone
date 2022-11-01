3da2dd31088b356ef5bd3038af0e14ff4177520c
Jideofor Ike <jideoforike@MacBook-Pro.local>
2022-11-01 21:40:13 +0100

    almost complete

--- src/componets/LinksPage.js
+++ src/componets/LinksPage.js
@@ -12,12 +12,12 @@ function LinksPage(){
                  title="Read more about failed elephant evacuation </br> this is working"><span>Twitter Link
                     </span></a></li>
              <li id='btn__zuri'>
-                <a target="_blank" rel="noreferrer"className="fcc-btn1" href="https://training.zuri.team/"
+                <a target="_blank" ref_id="<Harry>"rel="noreferrer"className="fcc-btn1" href="https://training.zuri.team/"
                  title="The zuri theme books"><span>Zuri Team</span></a></li>
-             <li id='books'>ref_id="Harry"<a target="_blank" rel="noreferrer"className="fcc-btn2" href="http://books.zuri.team"><span>Zuri Books</span><span className="subtext">this works</span></a></li>
-             <li id='book__python'><a target="_blank" rel="noreferrer"className="fcc-btn3" href="https://books.zuri.team"><span>Python Books</span><span className="subtext2">this works</span></a></li>
-             <li id='pitch'><a target="_blank" rel="noreferrer"className="fcc-btn4" href="https://background.zuri.team"><span>Background Check for Coders</span><span className="subtext3">this works</span></a></li>
-             <li id='book__design'><a target="_blank" rel="noreferrer"className="fcc-btn5" href="https://books.zuri.team/design-rules"><span>Design Books</span><span className="subtext4">this works</span></a></li>
+             <li id='books'><a target="_blank" rel="noreferrer"className="fcc-btn2" href="http://books.zuri.team"><span>Zuri Books</span><span className="subtext">This is where you find the worlds best design and coding books</span></a></li>
+             <li id='book__python'><a target="_blank" rel="noreferrer"className="fcc-btn3" href="https://books.zuri.team"><span>Python Books</span><span className="subtext2">Use this link and get 20% off and donate to my channel</span></a></li>
+             <li id='pitch'><a target="_blank" rel="noreferrer"className="fcc-btn4" href="https://background.zuri.team"><span>Background Check for Coders</span><span className="subtext3">This is the best site to check out upcoming software developers</span></a></li>
+             <li id='book__design'><a target="_blank" rel="noreferrer"className="fcc-btn5" href="https://books.zuri.team/design-rules"><span>Design Books</span><span className="subtext4">Use this link to access the free zuri design book</span></a></li>
              </ul>
         </div>
     )
